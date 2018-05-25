ecbb1a176318818bcf336346fd60205586728f26
Ruli-AlSe <ra.almanzaserrano@gmail.com>
2018-05-24 21:20:06 -0500

    creacion de formulario de login, creacion de srvicio signup y envio de usuario y recepcion de token de autenticacion.

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -4,28 +4,56 @@ import { Component, OnInit, } from '@angular/core';
  import { Router, ActivatedRoute, Params} from '@angular/router';
  //Router son las librerias del router para navegacion
  //Params para recoger parametros por URL
+ import { User } from '../../models/user';
+
+ import { UserService } from '../../services/user.service';
 
  //define parametros o metadatos para configuracion del componente
  @Component({
  	selector: 'login',
  	templateUrl: './login.component.html',
- 	styleUrls: ['./login.component.css']
+ 	styleUrls: ['./login.component.css'],
+ 	providers: [ UserService ]
  })
 
 
  export class LoginComponent implements OnInit 
  {
  	public title:string;
+ 	public user: User;
  	
  	//proposito principal es asignar valores a las variables
- 	constructor() 
+ 	constructor(private _userService: UserService) 
  	{
  		this.title = 'Iniciar Sesión';
+ 		this.user = new User(1, 'ROLE_USER', '', '', '', '');
  	}
 
  	ngOnInit()
  	{
  		console.log('login.component cargado correctamente');
+ 	}
+
+ 	onSubmit(form)
+ 	{
+ 		//console.log(this.user);
+
+ 		this._userService.signup(this.user).subscribe(
+ 			response => {
+ 				//token
+ 				console.log(response);
+
+ 				this._userService.signup(this.user, true).subscribe(
+ 						response => {
+ 							console.log(response)
+ 						},
+ 						error => {
+ 							console.log(<any>error);
+ 						});
+ 			},
+ 			error => {
+ 				console.log(<any>error);
+ 			});
  	}
  }
 
--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -32,4 +32,21 @@ export class UserService
 		//peticion AJAX por POST
 		return this._http.post(this.url+'register', params, {headers: headers});
 	}
+
+	signup(user, gettoken = null): Observable<any>
+	{
+		if (gettoken != null) 
+		{
+			user.gettoken = 'true';
+		}
+		//Obtener el objeto user y transformarlo a formate JSON
+		let json = JSON.stringify(user);
+		let params = 'json='+json;
+
+		//Establecer cabeceras
+		let headers = new HttpHeaders().set('Content-Type', 'application/x-www-form-urlencoded');
+
+		//peticion AJAX por POST
+		return this._http.post(this.url+'login', params, {headers: headers});
+	}
 }
